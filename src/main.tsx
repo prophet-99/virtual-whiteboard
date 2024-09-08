3e56c09d17f2e31899445fbe977623fe8ed47a19
prophet-99 <alexanderavilab@gmail.com>
2024-09-07 20:56:23 -0500

    feat: structure for github-pages

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client';
-
-import './styles.scss';
-import WhiteBoard from './app/WhiteBoard';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<WhiteBoard />);
